2ca5cec3be8dfbfd56bc5159de6f214b1a2227a2
vanlinh00 <vanlinh20192019@gmail.com>
2022-06-30 11:19:41 +0700

    fix bug

--- src/GiamDan.php
+++ src/GiamDan.php
@@ -7,6 +7,7 @@
     $IDLoai = isset($_SESSION['IDLoai']) ? $_SESSION['IDLoai'] : null;
     $brand = isset($_SESSION['brand']) ? $_SESSION['brand'] : null;
 
+
     if($brand != null)
     {
         $command = "SELECT * FROM `sanpham` WhERE brand = '".$brand."' ORDER BY giaSP DESC";
@@ -18,11 +19,16 @@
 	    else
             $command = "SELECT * FROM `sanpham` ORDER BY giaSP DESC";
     } 
-    
     //Giảm giá
     $querySuKien = "SELECT * FROM sukien WHERE IDTL = '".$IDLoai."'";
 
     $resultSuKien = mysqli_query(Connect(),$querySuKien);
+
+    if($rowSK = mysqli_fetch_array($resultSuKien))
+    {
+        global $tienGiam;
+        $tienGiam = $rowSK['tienGiam'];
+    }
 
     if($rowSK = mysqli_fetch_array($resultSuKien))
     {
